ab6a91519b998a38e0bfb734103a8b9022193750
celador@github.com <john.aaron.nelson@gmail.com>
2016-02-07 00:47:05 -0500

    add typings

---
+++ typings/browser/definitions/jsforce/jsforce.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for jsforce
+// Project: https://github.com/jsforce/jsforce
+// Definitions by: JohnAaronNelson JohnAaronNelson.com
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+// Compiled using typings@0.6.2
+// Source: https://raw.githubusercontent.com/typings/typed-jsforce/a7e422c5455e70292e5675a727d43a7b05fc3e58/index.d.ts
+declare module 'jsforce/index' {
+
+    interface jsforceExports {
+        // constructor(options: { enabled: boolean }): jsforce;
+
+        // styles: Styles<StyleElement>;
+        // supportsColor: boolean;
+        // hasColor(value: string): boolean;
+        // stripColor(value: string): string;
+    }
+
+    var jsforce: jsforceExports;
+
+    export = jsforce;
+}
