fde3d0e8575db567b066033bca16c189b7316432
John Nelson <john.aaron.nelson@gmail.com>
2016-06-20 13:12:20 -0400

    try catch for initializing ForceCode

--- src/extension.ts
+++ src/extension.ts
@@ -7,49 +7,56 @@ import * as parsers from './parsers';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): any {
-    'use strict';
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+  'use strict';
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  try {
     vscode.window.forceCode = new ForceService(context);
     console.log('ForceCode is now active!');
+  } catch (error) {
+    console.error(error);
+  }
 
+  if (vscode.window.forceCode !== undefined) {
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.showMenu', () => {
-        commands.showMenu(context);
+      commands.showMenu(context);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.executeAnonymous', () => {
-        commands.executeAnonymous(vscode.window.activeTextEditor.document, context);
+      commands.executeAnonymous(vscode.window.activeTextEditor.document, context);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.getLog', () => {
-        commands.getLog(context);
+      commands.getLog(context);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.compile', () => {
-        commands.compile(vscode.window.activeTextEditor.document, context);
+      commands.compile(vscode.window.activeTextEditor.document, context);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.open', () => {
-        commands.open(context);
+      commands.open(context);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.exportPackage', () => {
-        commands.retrieve();
+      commands.retrieve();
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.staticResource', () => {
-        commands.staticResource(context);
+      commands.staticResource(context);
     }));
 
     context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
-        const toolingType: string = parsers.getToolingType(textDocument);
-        if (toolingType && vscode.window.forceCode.config && vscode.window.forceCode.config.autoCompile === true) {
-            commands.compile(textDocument, context);
-        }
+      const toolingType: string = parsers.getToolingType(textDocument);
+      if (toolingType && vscode.window.forceCode.config && vscode.window.forceCode.config.autoCompile === true) {
+        commands.compile(textDocument, context);
+      }
     }));
+  }
 
-    // // // Peek Provider Setup
-    // // const peekProvider: any = new commands.PeekFileDefinitionProvider();
-    // // const definitionProvider: any = vscode.languages.registerDefinitionProvider(constants.PEEK_FILTER, peekProvider);
-    // // context.subscriptions.push(definitionProvider);
+
+  // // // Peek Provider Setup
+  // // const peekProvider: any = new commands.PeekFileDefinitionProvider();
+  // // const definitionProvider: any = vscode.languages.registerDefinitionProvider(constants.PEEK_FILTER, peekProvider);
+  // // context.subscriptions.push(definitionProvider);
 }
