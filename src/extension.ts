f71fd78008f4f9410860b447026f7d2adb7db036
John Nelson <john.aaron.nelson@gmail.com>
2016-07-12 00:29:24 -0400

    foo

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,61 @@
 import * as vscode from 'vscode';
 import {ForceService} from './services';
 import * as commands from './commands';
-import * as parsers from './parsers';
+// import * as parsers from './parsers';
 
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): any {
   'use strict';
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  try {
-    vscode.window.forceCode = new ForceService(context);
-    vscode.window.setStatusBarMessage(`ForceCode is now active`);
+  if (true) {
     context.subscriptions.push(vscode.commands.registerCommand('ForceCode.showMenu', () => {
-      commands.showMenu(context);
+      vscode.window.showInformationMessage('FORCECODE WORKED!!');
+      // commands.showMenu(context);
+    }));
+  } else {
+    // vscode.window.forceCode = new ForceService(context);
+    // vscode.window.setStatusBarMessage(`ForceCode is now active`);
+    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.showMenu', () => {
+      vscode.window.showErrorMessage('FORCECODE Failed');
+      // commands.showMenu(context);
     }));
 
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.executeAnonymous', () => {
-      commands.executeAnonymous(vscode.window.activeTextEditor.document, context);
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.getLog', () => {
-      commands.getLog(context);
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.compile', () => {
-      commands.compile(vscode.window.activeTextEditor.document, context);
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.open', () => {
-      commands.open(context);
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.exportPackage', () => {
-      commands.retrieve();
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ForceCode.staticResource', () => {
-      commands.staticResource(context);
-    }));
-
-    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
-      const toolingType: string = parsers.getToolingType(textDocument);
-      if (toolingType && vscode.window.forceCode.config && vscode.window.forceCode.config.autoCompile === true) {
-        commands.compile(textDocument, context);
-      }
-    }));
-  } catch (error) {
-    console.error(error);
-    vscode.window.showErrorMessage('OH NO FORCECODE FAILED');
-    vscode.window.showErrorMessage(error);
   }
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.showMenu', () => {
+  //   commands.showMenu(context);
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.executeAnonymous', () => {
+  //   commands.executeAnonymous(vscode.window.activeTextEditor.document, context);
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.getLog', () => {
+  //   commands.getLog(context);
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.compile', () => {
+  //   commands.compile(vscode.window.activeTextEditor.document, context);
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.open', () => {
+  //   commands.open(context);
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.exportPackage', () => {
+  //   commands.retrieve();
+  // }));
+
+  // context.subscriptions.push(vscode.commands.registerCommand('ForceCode.staticResource', () => {
+  //   commands.staticResource(context);
+  // }));
+
+  // context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
+  //   const toolingType: string = parsers.getToolingType(textDocument);
+  //   if (toolingType && vscode.window.forceCode.config && vscode.window.forceCode.config.autoCompile === true) {
+  //     commands.compile(textDocument, context);
+  //   }
+  // }));
 
 
 
