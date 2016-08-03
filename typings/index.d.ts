da87cd361f16f393452b2eeb270d77a9895c55a4
John Nelson <john.aaron.nelson@gmail.com>
2016-08-02 23:44:48 -0400

    remove mocha typings as they're in the npm module

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,7 +6,6 @@
 /// <reference path="globals/jsforce/jsforce.d.ts" />
 /// <reference path="globals/jszip/jszip.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/moment/moment.d.ts" />
 /// <reference path="globals/node-fetch/index.d.ts" />
 /// <reference path="globals/xml2js/index.d.ts" />
