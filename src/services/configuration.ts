925f61feb9ce3e89bab7ff6bfd7b9b6ccd33780a
John Aaron Nelson <john.aaron.nelson@gmail.com>
2017-06-06 08:51:44 -0700

    revert to npm3

--- src/services/configuration.ts
+++ src/services/configuration.ts
@@ -6,18 +6,24 @@ import * as path from 'path';
 var _: any = require('lodash');
 
 export default function getSetConfig(service?: forceCode.IForceService): Promise<Config> {
-    return new Promise(function (resolve, reject) {
-        var self: forceCode.IForceService = service || vscode.window.forceCode;
-        try {
-            self.config = _.extend(self.config || {}, fs.readJsonSync(vscode.workspace.rootPath + path.sep + 'force.json'));
-            if (typeof self.config === 'object' && !self.config.src) {
-                self.config.src = 'src';
-            }
-            self.workspaceRoot = `${vscode.workspace.rootPath}${path.sep}${self.config.src}`;
-            resolve(self.config);
-        } catch (err) {
-            self.config = {};
-            resolve(self.config);
-        }
-    });
+	return new Promise(function (resolve, reject) {
+		var self: forceCode.IForceService = service || vscode.window.forceCode;
+		if (!vscode.workspace.rootPath) {
+			throw { message: 'Open a Folder with VSCode' }
+		}
+		try {
+			self.config = _.extend(self.config || {}, fs.readJsonSync(vscode.workspace.rootPath + path.sep + 'force.json'));
+			if (typeof self.config === 'object' && !self.config.src) {
+				self.config.src = 'src';
+			}
+			self.workspaceRoot = `${vscode.workspace.rootPath}${path.sep}${self.config.src}`;
+			if (!fs.existsSync(self.workspaceRoot)) {
+				fs.mkdirSync(self.workspaceRoot);
+			}
+			resolve(self.config);
+		} catch (err) {
+			self.config = {};
+			resolve(self.config);
+		}
+	});
 }
