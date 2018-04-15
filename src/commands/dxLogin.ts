9d9a943f59d312f6c0db04d3f22b4a23725f1ed3
Brett Wagner <wagner_engines@yahoo.com>
2018-04-14 20:49:39 -0500

    Code cleanup/bug fixes

--- src/commands/dxLogin.ts
+++ src/commands/dxLogin.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export default async function dxLogin(context: vscode.ExtensionContext): Promise<any> {
+export default async function dxLogin(): Promise<any> {
     vscode.window.forceCode.statusBarItem.text = 'ForceCode: Logging into DX';
     try {
         await vscode.window.forceCode.dxCommands.login();
--- src/commands/dxLogout.ts
+++ src/commands/dxLogout.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export default async function dxLogout(context: vscode.ExtensionContext): Promise<any> {
+export default async function dxLogout(): Promise<any> {
     vscode.window.forceCode.statusBarItem.text = 'ForceCode: Logging out of DX';
     try {
         await vscode.window.forceCode.dxCommands.logout();
