f840d312b7296564d97809a38000686192d346ec
Brett Wagner <wagner_engines@yahoo.com>
2018-05-02 16:14:54 -0500

    Make the output look nice

--- src/commands/overallCoverage.ts
+++ src/commands/overallCoverage.ts
@@ -14,20 +14,18 @@ export default function getOverallCoverage() {
             if (res.records) {
                 var outputString: string = 'Class/Trigger Name';
                 // use this for formatting
-                var spaces: string = '                                                  ';
+                var spaces: string = '                                                       ';
                 outputString += spaces.substr(0, spaces.length - outputString.length - 1) + 'Percent\t\t\tCovered/Total\n\n';
                 res.records.forEach(function(curRes: forceCode.ICodeCoverage) {
                     var uncovered: number = curRes.NumLinesUncovered;
                     var total: number = curRes.NumLinesCovered + curRes.NumLinesUncovered;
                     var percent = (((total - uncovered) / total) * 100).toFixed(2) + '% covered';
-                    outputString += curRes.ApexClassOrTrigger.Name + spaces.substr(0, spaces.length - curRes.ApexClassOrTrigger.Name.length - 1) + percent + '\t\t\t' + uncovered + '/' + total + '\n';
+                    outputString += curRes.ApexClassOrTrigger.Name + spaces.substr(0, spaces.length - curRes.ApexClassOrTrigger.Name.length - 1) + percent + spaces.substr(0, 20 - percent.length) + uncovered + '/' + total + '\n';
                 });
-                var filename = 'ApexCoverage' + Date.now() + '.txt';
-                var wholeFilename = `${vscode.window.forceCode.workspaceRoot}${path.sep}coverage${path.sep}${filename}`;
-                return vscode.window.forceCode.dxCommands.saveToFile(outputString, 'coverage' + path.sep + filename).then(ret =>{
+                return vscode.window.forceCode.dxCommands.saveToFile(outputString, 'coverage' + path.sep + 'ApexCoverage-' + Date.now() + '.txt').then(filename =>{
                     vscode.window.forceCode.statusBarItem.text = 'ForceCode: Code coverage retrieval complete!';
                     vscode.window.forceCode.resetMenu();
-                    return vscode.workspace.openTextDocument(wholeFilename).then(doc => vscode.window.showTextDocument(doc, 3));
+                    return vscode.workspace.openTextDocument(filename).then(doc => vscode.window.showTextDocument(doc, 3));
                 });
             }
         });
