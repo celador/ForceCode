172d08799e6307fcfab7664ec37e392804c9a7b4
Brett Wagner <wagner_engines@yahoo.com>
2018-05-08 13:12:42 -0500

    Update the lastModifiedById with the correct Id instead of the same thing

--- src/forceCode.d.ts
+++ src/forceCode.d.ts
@@ -76,6 +76,28 @@ export interface MetadataResult {
     SystemModstamp: string;
 }
 
+export interface MetadataResult {
+    ApiVersion: number;
+    attributes: { type: string };
+    Body: string;
+    BodyCrc: number;
+    CreatedById: string;
+    CreatedDate: string;
+    FullName: string;
+    Id: string;
+    IsValid: boolean;
+    LastModifiedById: string;
+    LastModifiedDate: string;
+    LengthWithoutComments: number;
+    ManageableState: string;
+    Metadata: {};
+    Name: string;
+    NamespacePrefix: string;
+    Status: string;
+    SymbolTable: {};
+    SystemModstamp: string;
+}
+
 interface ILocationsNotCovered {
     column: Number;
     line: Number;
