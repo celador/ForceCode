015d38250a50cd7047b4d899e5d15c4047ef6ff8
Brett Wagner <bwagner@vermillion.eaglecrk.local>
2018-06-08 12:46:35 -0500

    Fix issue of wsMembers file becoming corrupted when multiple files are deleted

--- src/extension.ts
+++ src/extension.ts
@@ -76,6 +76,7 @@ export function activate(context: vscode.ExtensionContext): any {
     // watch for config file changes
     context.subscriptions.push(vscode.workspace.createFileSystemWatcher(path.join(vscode.window.forceCode.workspaceRoot, 'force.json')).onDidChange(uri => { configuration() }));
     
+    var timeO;
     // watch for deleted files and update workspaceMembers
     context.subscriptions.push(vscode.workspace.createFileSystemWatcher(path.join(vscode.window.forceCode.projectRoot, '**', '*.{cls,trigger,page,component,cmp}')).onDidDelete(uri => {
         const fcfile: FCFile = codeCovViewService.findByPath(uri.fsPath);
