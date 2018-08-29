4698832693cd573d7d02dc6a9a588a7c63d62500
Brett <daishi4u@yahoo.com>
2018-08-28 21:54:17 -0500

    Create symlinks for sfdx faux classes to the project src file so we can have the proper code completion for each org

--- src/services/configuration.ts
+++ src/services/configuration.ts
@@ -9,20 +9,10 @@ import { switchUserViewService } from '.';
 export default function getSetConfig(service?: forceCode.IForceService): Promise<Config> {
 	return new Promise(function (resolve) {
 		var self: forceCode.IForceService = service || vscode.window.forceCode;
-		const projPath = vscode.workspace.workspaceFolders[0].uri.fsPath + path.sep;
-		if(!fs.existsSync(projPath + 'sfdx-project.json')) {
-			// add in a bare sfdx-project.json file for language support from official salesforce extensions
-			const sfdxProj: {} = {
-				namespace: "", 
-				sfdcLoginUrl: 'https://login.salesforce.com/', 
-				sourceApiVersion: constants.API_VERSION,
-			};
-			
-			fs.outputFileSync(projPath + 'sfdx-project.json', JSON.stringify(sfdxProj, undefined, 4));
-		}
 		if (!vscode.workspace.workspaceFolders) {
 			throw new Error('Open a Folder with VSCode before trying to login to ForceCode');
 		}
+		const projPath = vscode.workspace.workspaceFolders[0].uri.fsPath + path.sep;
 		try {
 			self.config = fs.readJsonSync(projPath + 'force.json');
 		} catch (err) {
--- src/services/forceService.ts
+++ src/services/forceService.ts
@@ -144,7 +144,7 @@ export default class ForceService implements forceCode.IForceService {
             klaw(vscode.window.forceCode.workspaceRoot)
                 .on('data', function (item) {
                     // Check to see if the file represents an actual member... 
-                    if (item.stats.isFile()) {                        
+                    if (item.stats.isFile() && item.path.indexOf('.sfdx') === -1) {                        
                         var type: string = getToolingTypeFromExt(item.path);
 
                         if(type) {
