32a80c419f1b23476216d7585db2ba57b20c1104
Brett <daishi4u@yahoo.com>
2018-08-31 21:58:06 -0500

    Fix date compare method

--- src/services/codeCovView.ts
+++ src/services/codeCovView.ts
@@ -244,8 +244,8 @@ import {
       if(!this.wsMember.lastModifiedDate) {
         return true;
       }
-      var localMS: number = fs.statSync(this.wsMember.path).mtimeMs;
-      var serverMS: number = (new Date(serverDate)).getTime();
+      var localMS: number = (new Date(this.wsMember.lastModifiedDate)).getUTCMilliseconds();
+      var serverMS: number = (new Date(serverDate)).getUTCMilliseconds();
 
       if(localMS > serverMS || serverMS - localMS <= constants.MAX_TIME_BETWEEN_FILE_CHANGES) {
           return true;
