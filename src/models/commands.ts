a588ddee6ceabacdd2aa5445666cb039afda6857
Brett <daishi4u@yahoo.com>
2018-09-07 18:16:21 -0500

    Throw an error if no file is open when trying to save through the menu. Disable commands when not logged in. Disable certain commands when no file is open

--- src/models/commands.ts
+++ src/models/commands.ts
@@ -154,7 +154,7 @@ export const fcCommands: FCCommand[] = [
                     .then(doc => { return commands.compile(doc); });
             }
             if(!vscode.window.activeTextEditor) {
-                return undefined;
+                return;
             }
             return commands.compile(vscode.window.activeTextEditor.document);
         }
