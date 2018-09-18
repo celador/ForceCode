bfa2e812f75712c0e21f2ccc44f0aaa978313e46
Brett <daishi4u@yahoo.com>
2018-09-17 22:34:05 -0500

    Finish implementing a simple settings gui

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -4,30 +4,37 @@ import * as path from 'path';
 import * as deepmerge from 'deepmerge'
 
 export default function settings(): Promise<any> {
-    const myExtDir = vscode.extensions.getExtension ("BrettWagner.forcecode").extensionPath;
+    const myExtDir = vscode.extensions.getExtension("BrettWagner.forcecode").extensionPath;
     const SETTINGS_FILE: string = path.join(myExtDir, 'pages', 'settings.html');
-    const panel = vscode.window.createWebviewPanel('fcSettings', "ForceCode Settings", vscode.ViewColumn.One, { 
+    const panel = vscode.window.createWebviewPanel('fcSettings', "ForceCode Settings", vscode.ViewColumn.One, {
         enableScripts: true,
         retainContextWhenHidden: true
-     });
+    });
+
+    var tempSettings = {};
 
     // And set its HTML content
     panel.webview.html = getSettingsPage();
 
     panel.webview.postMessage(vscode.window.forceCode.config);
 
+    const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray;
+
     // handle settings changes
     panel.webview.onDidReceiveMessage(message => {
-        vscode.window.forceCode.config = deepmerge(vscode.window.forceCode.config, message);
+        if (message.delete) {
+            delete vscode.window.forceCode.config.srcs[message.delete];
+            delete tempSettings[message.delete];
+        } else if (message.save) {
+            vscode.window.forceCode.config = deepmerge(vscode.window.forceCode.config, tempSettings, { arrayMerge: overwriteMerge });
+            fs.outputFileSync(path.join(vscode.window.forceCode.workspaceRoot, 'force.json'), JSON.stringify(vscode.window.forceCode.config, undefined, 4));
+            vscode.window.showInformationMessage('ForceCode settings saved successfully!', 'OK');
+        } else {
+            tempSettings = deepmerge(tempSettings, message, { arrayMerge: overwriteMerge });
+        }
     }, undefined);
 
-    // save the settings on close
-    panel.onDidDispose(() => {
-        fs.outputFileSync(path.join(vscode.window.forceCode.workspaceRoot, 'force.json'), JSON.stringify(vscode.window.forceCode.config, undefined, 4));
-    });
-
     return Promise.resolve();
-
 
     function getSettingsPage(): string {
         return fs.readFileSync(SETTINGS_FILE).toString();
