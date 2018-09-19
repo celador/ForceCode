5d2422216129309c698f8d4924b82a43d03ad512
Brett <daishi4u@yahoo.com>
2018-09-18 19:02:52 -0500

    Update lastModifiedDate after a deploy

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
-import { dxService } from '../services';
+import { dxService, codeCovViewService, fcConnection } from '../services';
 import { getFileListFromPXML, zipFiles } from './../services';
+import * as path from 'path';
+import { FCFile } from '../services/codeCovView';
+import { IWorkspaceMember } from '../forceCode';
 
 export default function deploy(context: vscode.ExtensionContext) {
     vscode.window.forceCode.outputChannel.clear();
@@ -14,38 +17,41 @@ export default function deploy(context: vscode.ExtensionContext) {
         allowMissingFiles: true,
     };
 
-    return Promise.resolve(vscode.window.forceCode)
-        .then(deployPackage)
-        .then(finished);
-    // =======================================================================================================================================
-    // =======================================================================================================================================
-    function deployPackage() {
-        return getFileListFromPXML().then(files => {
-            var zip = zipFiles(files, deployPath);
-            Object.assign(deployOptions, vscode.window.forceCode.config.deployOptions);
-            vscode.window.forceCode.outputChannel.show();
-            return vscode.window.forceCode.conn.metadata.deploy(zip, deployOptions)
-                .complete(function (err, result) {
-                    if (err) {
-                        return err;
-                    } else {
-                        return result;
+    return getFileListFromPXML().then(files => {
+        var zip = zipFiles(files, deployPath);
+        Object.assign(deployOptions, vscode.window.forceCode.config.deployOptions);
+        vscode.window.forceCode.outputChannel.show();
+        return vscode.window.forceCode.conn.metadata.deploy(zip, deployOptions)
+            .complete(function (err, result) {
+                if (err) {
+                    return err;
+                } else {
+                    return result;
+                }
+            })
+            .then(finished);
+
+
+        // =======================================================================================================================================
+        function finished(res) /*Promise<any>*/ {
+            if (res.status !== 'Failed') {
+                // update the wsMembers with the newer date
+                files.forEach(file => {
+                    const curFCFile: FCFile = codeCovViewService.findByPath(path.join(deployPath, file));
+                    if(curFCFile) {
+                        const wsMem: IWorkspaceMember = curFCFile.getWsMember();
+                        wsMem.lastModifiedDate = (new Date()).toISOString();
+                        wsMem.lastModifiedById = fcConnection.currentConnection.orgInfo.userId;
+                        curFCFile.updateWsMember(wsMem);
                     }
                 });
-        });
-    }
-    // =======================================================================================================================================
-    function finished(res) /*Promise<any>*/ {
-        if (res.status !== 'Failed') {
-            vscode.window.forceCode.showStatus('ForceCode: Deployed $(thumbsup)');
-        } else {
-            vscode.window.showErrorMessage('ForceCode: Deploy Errors');
+                vscode.window.forceCode.showStatus('ForceCode: Deployed $(thumbsup)');
+            } else {
+                vscode.window.showErrorMessage('ForceCode: Deploy Errors');
+            }
+            vscode.window.forceCode.outputChannel.append(dxService.outputToString(res).replace(/{/g, '').replace(/}/g, ''));
+            return res;
         }
-        vscode.window.forceCode.outputChannel.append(dxService.outputToString(res).replace(/{/g, '').replace(/}/g, ''));
-        return res;
-    }
-    // =======================================================================================================================================
+        // =======================================================================================================================================
+    });
 }
-
-
-
