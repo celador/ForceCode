68faac4d3cacf71deaa195b797c68c39430a9433
Brett Wagner <daishi4u@yahoo.com>
2019-07-09 23:38:23 -0500

    Add number of lines covered in Forcecode Code Coverage view hovers

--- src/services/codeCovView.ts
+++ src/services/codeCovView.ts
@@ -281,6 +281,7 @@ export class FCFile extends TreeItem {
     if (this.wsMember.coverage) {
       var fileCoverage: ICodeCoverage = this.wsMember.coverage;
       var total: number = fileCoverage.NumLinesCovered + fileCoverage.NumLinesUncovered;
+      this.tooltip = fileCoverage.NumLinesCovered + '/' + total + ' lines covered';
       var percent = Math.floor((fileCoverage.NumLinesCovered / total) * 100);
       this.label = percent + '% ' + this.label;
       const imagePath: string = path.join(window.forceCode.storageRoot, 'images');
