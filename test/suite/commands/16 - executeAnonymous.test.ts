bab57bee72bf6631373bfa3939372740d07e0b04
Brett Wagner <daishi4u@yahoo.com>
2019-07-20 14:41:38 -0500

    Fix execute anonymous test

--- test/suite/commands/16 - executeAnonymous.test.ts
+++ test/suite/commands/16 - executeAnonymous.test.ts
@@ -1,27 +1,25 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
+import { saveToFile } from '../../../src/util';
 
 suite('executeAnonymous.ts', () => {
   test('Executes apex code', async () => {
-    const newDocURI: vscode.Uri = vscode.Uri.parse(`untitled:${new Date().toISOString()}.cls`);
-    await vscode.workspace
-      .openTextDocument(newDocURI)
-      .then(function(_document: vscode.TextDocument) {
-        return vscode.window.showTextDocument(_document, 3, false).then(editor => {
-          const testText = "System.debug('hello');";
-          editor.edit(edit => {
-            edit.insert(new vscode.Position(0, 0), testText);
-          });
+    const testText = "System.debug('hello');";
+    await saveToFile(testText, Date.now() + '.cls').then(filename => {
+      return vscode.workspace
+        .openTextDocument(filename)
+        .then(doc => vscode.window.showTextDocument(doc, 3))
+        .then(editor => {
           editor.selections = [
             new vscode.Selection(
               new vscode.Position(0, 0),
-              new vscode.Position(0, testText.length - 1)
+              new vscode.Position(0, testText.length)
             ),
           ];
           return vscode.commands.executeCommand('ForceCode.executeAnonymous').then(res => {
             assert.strictEqual(true, true);
           });
         });
-      });
+    });
   });
 });
