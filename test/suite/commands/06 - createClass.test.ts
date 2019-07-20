635c2d43e3e9ede09d0f2aa81b47834bed399b35
Brett Wagner <daishi4u@yahoo.com>
2019-07-20 15:47:44 -0500

    Unit test createClass, executeAnonymous

--- test/suite/commands/06 - createClass.test.ts
+++ test/suite/commands/06 - createClass.test.ts
@@ -25,53 +25,139 @@ suite('createClass.ts', () => {
         },
       };
     });
-    await vscode.commands.executeCommand('ForceCode.createClass').then(res => {
-      var output = path.join(vscode.window.forceCode.projectRoot, 'classes', 'testerson.cls');
-      assert.strictEqual(fs.existsSync(output), true);
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(vscode.window.forceCode.projectRoot, 'classes', 'testerson.cls');
+        assert.strictEqual(fs.existsSync(output), true);
+      });
     });
   });
   test('Creates Visualforce Page', async () => {
     sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items, options) {
       return {
         async then(callback) {
-          return callback(items[4]); // apex class
+          return callback(items[4]); // VF page
         },
       };
     });
     sandbox.stub(vscode.window, 'showInputBox').callsFake(function(options) {
       return {
         async then(callback) {
-          return callback('testerson'); // name of class
+          return callback('testerson'); // name of VF page
         },
       };
     });
-    await vscode.commands.executeCommand('ForceCode.createClass').then(res => {
-      var output = path.join(vscode.window.forceCode.projectRoot, 'pages', 'testerson.page');
-      assert.strictEqual(fs.existsSync(output), true);
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(vscode.window.forceCode.projectRoot, 'pages', 'testerson.page');
+        assert.strictEqual(fs.existsSync(output), true);
+      });
     });
   });
   test('Creates Visualforce Component', async () => {
     sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items, options) {
       return {
         async then(callback) {
-          return callback(items[5]); // apex class
+          return callback(items[5]); // VF component
         },
       };
     });
     sandbox.stub(vscode.window, 'showInputBox').callsFake(function(options) {
       return {
         async then(callback) {
-          return callback('testerson'); // name of class
+          return callback('testerson'); // name of component
         },
       };
     });
-    await vscode.commands.executeCommand('ForceCode.createClass').then(res => {
-      var output = path.join(
-        vscode.window.forceCode.projectRoot,
-        'components',
-        'testerson.component'
-      );
-      assert.strictEqual(fs.existsSync(output), true);
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(
+          vscode.window.forceCode.projectRoot,
+          'components',
+          'testerson.component'
+        );
+        assert.strictEqual(fs.existsSync(output), true);
+      });
+    });
+  });
+  test('Creates Aura App', async () => {
+    sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items, options) {
+      return {
+        async then(callback) {
+          return callback(items[0]); // Aura, app
+        },
+      };
+    });
+    sandbox.stub(vscode.window, 'showInputBox').callsFake(function(options) {
+      return {
+        async then(callback) {
+          return callback('testersonAura'); // name of Aura app
+        },
+      };
+    });
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(vscode.window.forceCode.projectRoot, 'aura', 'testersonAura');
+        assert.strictEqual(fs.existsSync(output), true);
+      });
+    });
+  });
+  test('Creates Trigger on Account', async () => {
+    sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items, options) {
+      return {
+        async then(callback) {
+          return callback(items[3]); // Trigger
+        },
+      };
+    });
+    var inputStub = sandbox
+      .stub(vscode.window, 'showInputBox')
+      .onFirstCall()
+      .callsFake(function(options) {
+        return {
+          async then(callback) {
+            return callback('testerson'); // name of Trigger
+          },
+        };
+      });
+    inputStub.onSecondCall().callsFake(function(options) {
+      return {
+        async then(callback) {
+          return callback('Account'); // Trigger is on Object Account
+        },
+      };
+    });
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(
+          vscode.window.forceCode.projectRoot,
+          'triggers',
+          'testerson.trigger'
+        );
+        assert.strictEqual(fs.existsSync(output), true);
+      });
+    });
+  });
+  test('Creates LWC Component', async () => {
+    sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items, options) {
+      return {
+        async then(callback) {
+          return callback(items[2]); // LWC component
+        },
+      };
+    });
+    sandbox.stub(vscode.window, 'showInputBox').callsFake(function(options) {
+      return {
+        async then(callback) {
+          return callback('testersonLWC'); // name of component
+        },
+      };
+    });
+    await vscode.commands.executeCommand('ForceCode.createClass').then(async res => {
+      await vscode.commands.executeCommand('ForceCode.compile').then(res => {
+        var output = path.join(vscode.window.forceCode.projectRoot, 'lwc', 'testersonLWC');
+        assert.strictEqual(fs.existsSync(output), true);
+      });
     });
   });
 });
