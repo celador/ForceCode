bf157c527cb086b84051c7e5568b4cb87e8effd4
Brett Wagner <daishi4u@yahoo.com>
2021-10-27 23:05:09 -0500

    Fix runTest issues

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 async function main() {
   try {
